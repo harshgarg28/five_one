359cf379f88bfb88e44e988c522ada40b78531cc
Harsh Garg <harshgargckt@gmail.com>
2024-04-18 12:30:00 +0530

    adding new operations

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request
 
 from .extensions import db
 from .models import User
@@ -8,11 +8,26 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     users = User.query.all()
-    users_list_html = [f"<li>{ user.username }</li>" for user in users]
+    users_list_html = [f"<li>{user.username} [<a href='/delete/{user.id}'>delete</a>]</li>" for user in users]
     return f"<ul>{''.join(users_list_html)}</ul>"
 
 @main.route('/add/<username>')
 def add_user(username):
     db.session.add(User(username=username))
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+@main.route('/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+@main.route('/update/<int:user_id>', methods=['POST'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    new_username = request.form.get('new_username')
+    user.username = new_username
     db.session.commit()
     return redirect(url_for("main.index"))
